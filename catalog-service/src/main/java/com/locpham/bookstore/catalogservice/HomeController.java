68763227fc139aaf0b5c5a5f79e5a1ee5ac9ac52
Loc Pham <halfbloodprincess9@gmail.com>
2026-03-10 20:55:33 +0700

    check ci cd 2

--- catalog-service/src/main/java/com/locpham/bookstore/catalogservice/HomeController.java
+++ catalog-service/src/main/java/com/locpham/bookstore/catalogservice/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "Welcome the catalog book a";
+        return "Welcome the catalog book";
     }
 }
